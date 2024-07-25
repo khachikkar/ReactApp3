c5113bbe7ec59a5988f6ab17dadd93dbff09d85e
khachikkar <khachkarapetyan@Khachs-MacBook-Air.local>
2024-07-25 13:03:03 +0400

    updated nav bar and footer

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../img/logo.svg";
 import "./Nav.css";
+import { IoClose } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+
+
+import { useRef } from "react";
 
 export default function Nav() {
-  const [menuVisible, setMenuVisible] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
+const navRef = useRef()
+
+const shownavbar =()=>{
+  navRef.current.classList.toggle("responsive_nav")
+}
 
   return (
     <div className="navbar">
@@ -17,30 +22,23 @@ export default function Nav() {
           <img className="logo" src={logo} alt="logo" />
         </Link>
       </div>
-      <li id="little" onClick={toggleMenu}>
-        Menu
-      </li>
-      {menuVisible && (
-        <div className="menu-overlay">
-          <div className="menu-content">
-            <ul>
-              <li>Appartements</li>
-              <li>Contact Us</li>
-              <li onClick={toggleMenu}>
-                <Link className="lil" to="/src/Images">
-                  Images
-                </Link>
-              </li>
-              <li>
-                <button className="primaryButton">Get Start</button>
-              </li>
-            </ul>
-            <button className="close-menu" onClick={toggleMenu}>Close</button>
-          </div>
-        </div>
-      )}
-      <button id="little-btn" className="primaryButton">
-        Get Start
+
+      <ul ref={navRef}>
+        <li>Appartements</li>
+        <li>Contact Us</li>
+        <li onClick={shownavbar}>
+          <Link to="/src/Images">Images</Link>
+        </li>
+        <li>
+          <button className="primaryButton">Get Start</button>
+        </li>
+        <button onClick={shownavbar} className="nv-btn close">
+          <IoClose className="iicon" />
+        </button>
+      </ul>
+
+      <button onClick={shownavbar} className="nv-btn">
+        <IoMenu className="iicon" />
       </button>
     </div>
   );
--- src/Images.js
+++ src/Images.js
@@ -1,19 +1,64 @@
-import React from 'react';
-import "./Images.css"
+import React from "react";
+import "./Images.css";
+import { useState, useEffect } from "react";
+import GetStarted from "./Components/GetStart/GetStart"
+const key = "e-2M5SW_iMn3Ky4P0eJ5qd88qw30QLrBTas9xQ9VmP4";
 
 function Images() {
+  const [res, setRes] = useState("");
+  const [searchval, SetSearchval] = useState("");
+  const [data, setData] = useState([]);
+  const [rres, setRres] = useState("Search for Inspiration");
+
+  const handle = () => {
+    SetSearchval(res);
+    setRes("");
+  };
+
+  useEffect(() => {
+    if (searchval) {
+      fetch(
+        `https://api.unsplash.com/search/photos/?query=${searchval}&client_id=${key}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data.results);
+          setRres("");
+        });
+    }
+  }, [searchval]);
+
   return (
-    <section className='images-sec'>
+    <section className="images-sec">
+      <div className="ads">
+        <h1>Get Inspirations Here</h1>
+      </div>
 
+      <div className="i-search">
+        <input
+          onChange={(e) => {
+            setRes(e.target.value);
+          }}
+          className="Input"
+          value={res}
+          placeholder="Seach any image that you wanna"
+        />
+        <button onClick={handle} className="primaryButton">
+          Search
+        </button>
+      </div>
 
-<div className='ads'>
-      <h1>Images Page</h1>
-      <p>This is the Images page.</p>
-</div>
+      <h3>{searchval}</h3>
 
+      <div className="i-results">
+        <span>{rres}</span>
+        {data.map((item) => {
+          return <img className="i-img" key={item.id} src={item.urls.full} />;
+        })}
+      </div>
 
+      <GetStarted />
     </section>
-    
   );
 }
 
